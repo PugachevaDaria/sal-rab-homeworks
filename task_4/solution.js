32f36eae1863141ddcf63b4d431e676a6b7ea2c8
Daria Pugacheva <darnik1906@gmai.com>
2021-11-18 20:18:18 +0300

    task_4 done_2

--- task_4/solution.js
+++ task_4/solution.js
@@ -7,9 +7,7 @@ function parseProducts(json) {
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
-}
-let data = JSON.parse(json);
-return data.products;
+
 
 // Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
