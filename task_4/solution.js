e7417346a4c05c84e33b8f13e9a138da8b6996ad
Daria Pugacheva <darnik1906@gmai.com>
2021-11-20 15:19:41 +0300

    task_4 done_3

--- task_4/solution.js
+++ task_4/solution.js
@@ -7,6 +7,9 @@ function parseProducts(json) {
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
+  }
+  let data = JSON.parse(json);
+  return data.products;
 
 
 // Напишите функцию renderProductsCards(json)
@@ -18,16 +21,16 @@ function parseProducts(json) {
 // и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
 
 function renderProductsCards(json) {
+}
+ 
+function renderProductsCards(json) {
   clearProducts();
   let products = parseProducts(json);
-  function parseProducts(json) {
-  let data = JSON.parse(json);
-  return data.products;
-  }
   let length = products.length;
-  for (let i = 0; i < length; i++) {
-  addProduct(products[i]);
-  }
-    }
+} 
+  for (let i = 0; i < length; i += 1) {
+      addProduct(products[i]);
+  } 
+
   
 
